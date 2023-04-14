fab8d197973d00338824c1a9681ccc4c89bc01be
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-04-14 17:37:23 +0200

    add conditions

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,30 @@
 
 const eleGrid = document.querySelector('.grid');
 
-for (let i = 0; i <= 100; i++){
-    console.log(i);
-    eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`
+
+for (let i = 1; i <= 100; i++){
+   
+    if(i % 3 === 0 && i % 5 === 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + 
+        `<div class="cell">FizzBuzz</div>`
+        console.log('FizzBuz')
+    }
+
+    else if (i % 3 === 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + 
+        `<div class="cell">Fizz</div>`
+        console.log('Fizz')
+    }
+
+    else if (i % 5 === 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + 
+        `<div class="cell">Buzz</div>`
+        console.log('Buzz')
+    }
+
+    else{
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`
+        console.log(i);
+    }
 }
+
